70d3cb18eeb8b16121986aac6295e0b8046196e8
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-01 02:22:51 -0500

    changed file paths in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Nav from '../Components/Nav';
-import Home from '../Components/Home';
-import About from '../Components/About';
-import Projects from '../Components/Projects';
-import Contact from '../Components/Contact';
+import Nav from './Components/Nav';
+import Home from './Components/Home';
+import About from './Components/About';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
